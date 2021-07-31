230a67eeb4641148c3cbf0648cb5227b5a7c4311
hoo ws <jiadexx@gmail.com>
2021-07-31 12:01:27 +0800

    authentication stuff

---
+++ src/helpers/jwt.helper.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const { setJWT, getJWT } = require("./redis.helper");
+const { storeUserRefreshJWT } = require("../model/user/User.model");
+const { token } = require("morgan");
+
+const crateAccessJWT = async (email, _id) => {
+    try {
+      const accessJWT = await jwt.sign({ email }, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "1d", //change this to 15m
+      });
+  
+      await setJWT(accessJWT, _id);
+  
+      return Promise.resolve(accessJWT);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+const crateRefreshJWT = async (email, _id) => {
+    try {
+      const refreshJWT = jwt.sign({ email }, process.env.JWT_REFRESH_SECRET, {
+        expiresIn: "30d",
+      });
+  
+      await storeUserRefreshJWT(_id, refreshJWT);
+  
+      return Promise.resolve(refreshJWT);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  
+
+  module.exports = {
+    crateAccessJWT,
+    crateRefreshJWT,
+  };
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { hashPassword } = require("../helpers/bcrypt.helper");
+const { hashPassword,comparePassword } = require("../helpers/bcrypt.helper");
+const { crateAccessJWT, crateRefreshJWT } = require("../helpers/jwt.helper");
 const router = express.Router();
 
 const {
 	insertUser,
+    getUserByEmail
 } = require('../model/user/User.model');
 
 router.post("/", async (req, res) => {
@@ -25,6 +27,47 @@ router.post("/", async (req, res) => {
         console.log(error);
         res.json({status:'error',message:error.message});
     }
+});
+
+router.post("/login", async (req, res) => {
+	console.log(req.body);
+
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		return res.json({ status: "error", message: "Invalid form submition!" });
+	}
+
+	const user = await getUserByEmail(email);
+
+	if (!user.isVerified) {
+		return res.json({
+			status: "error",
+			message:
+				"You account has not been verified. Please check your email and verify you account before able to login!",
+		});
+	}
+
+	const passFromDb = user && user._id ? user.password : null;
+
+	if (!passFromDb)
+		return res.json({ status: "error", message: "Invalid email or password!" });
+
+	const result = await comparePassword(password, passFromDb);
+
+	if (!result) {
+		return res.json({ status: "error", message: "Invalid email or password!" });
+	}
+
+	const accessJWT = await crateAccessJWT(user.email, `${user._id}`);
+	const refreshJWT = await crateRefreshJWT(user.email, `${user._id}`);
+
+	res.json({
+		status: "success",
+		message: "Login Successfully!",
+		accessJWT,
+		refreshJWT,
+	});
 });
 
 
